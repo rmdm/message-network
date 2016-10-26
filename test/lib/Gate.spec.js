2765a8dc567cb93bc969d4fdb100f678fd86822a
d <d@dpc>
2016-10-26 19:55:38 +0300

    gate initial

--- test/lib/Gate.spec.js
+++ test/lib/Gate.spec.js
@@ -3,4 +3,16 @@ describe('Gate class', function () {
     var assert = require('assert')
     var Gate = require('../../lib/Gate')
 
+    describe('transfer method', function () {
+
+        it('throws an error', function () {
+            var gate = Gate()
+
+            assert.throws(function () {
+                gate.transfer({})
+            })
+        })
+
+    })
+
 })
