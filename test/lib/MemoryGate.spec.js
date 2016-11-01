149f8fb926e14d7aca7d0d7445c30df828f007e7
rmdm <rmdm@users.noreply.github.com>
2016-11-01 19:23:23 +0300

    MemoryGate

---
+++ test/lib/MemoryGate.spec.js
@@ -0,0 +1,39 @@
+describe('MemoryGate class', function () {
+
+    var assert = require('assert')
+    var sinon = require('sinon')
+
+    var MemoryGate = require('../../lib/MemoryGate')
+
+    describe('link method', function () {
+
+        it('accepts other gate and sets each other as endpoints', function () {
+            var gate = MemoryGate()
+            var anotherGate = MemoryGate()
+
+            gate.link(anotherGate)
+
+            assert.equal(gate._endpoint, anotherGate)
+            assert.equal(anotherGate._endpoint, gate)
+        })
+
+    })
+
+    describe('_transfer method', function () {
+
+        it('calls "receive" of its endpoint', function () {
+            var gate = MemoryGate()
+            var anotherGate = MemoryGate()
+
+            sinon.stub(anotherGate, 'receive')
+
+            gate.link(anotherGate)
+
+            gate._transfer({data: 'data'})
+
+            assert(anotherGate.receive.calledWithMatch({data: 'data'}))
+        })
+
+    })
+
+})
